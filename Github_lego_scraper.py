55c6f8f3a38311835db262c463b95725a9f8718f
dorbei0901 <dorbei0901@gmail.com>
2026-02-27 23:28:00 -0800

    Adjust minimum discount and price thresholds

--- Github_lego_scraper.py
+++ Github_lego_scraper.py
@@ -360,8 +360,8 @@ def scrape_amazon_lego_selenium(keyword="", min_discount_percent=30.0, min_origi
 def main():
     print("🔎 Amazon LEGO Discount Scraper (GitHub Actions Edition)")
     
-    min_discount_percent = 30 
-    min_original_price = 50
+    min_discount_percent = 25 
+    min_original_price = 40
     amazon_tag = os.getenv('AMAZON_TAG', '')
 
     themes = load_lego_themes()
