99b97eaa1cc2e9040e6603f2841f36f083e26341
dorbei0901 <262217419+dorbei0901@users.noreply.github.com>
2026-07-29 00:39:01 -0700

    Update Github_lego_Amazon_scraper.py

--- Github_lego_Amazon_scraper.py
+++ Github_lego_Amazon_scraper.py
@@ -396,7 +396,7 @@ def main():
     print("🔎 Amazon LEGO Discount Scraper (GitHub Actions Edition)")
     
     min_discount_percent = 20
-    min_original_price = 10
+    min_original_price = 50
     amazon_tag = os.getenv('AMAZON_TAG', '')
 
     themes = load_lego_themes()
